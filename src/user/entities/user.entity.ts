f53848d0997ab1da40701a403451cf10fda9f8e3
Abraham <1001.28021547.ucla@gmail.com>
2025-07-03 14:58:07 -0400

    add user entity and base entity

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,40 @@
+import { BaseModel } from 'src/utils/entity';
+import { Column, Entity } from 'typeorm';
+
+export enum UserRole {
+  MAIN = 'main',
+  SUPER_ADMIN = 'super_admin',
+  ADMIN = 'admin',
+  VENDOR = 'vendor',
+  CUSTOMER = 'customer',
+}
+
+@Entity('user')
+export class User extends BaseModel {
+  @Column({ type: 'character varying', name: 'code', unique: true })
+  code: string;
+
+  @Column({ type: 'character varying', name: 'first_name' })
+  firstName: string;
+
+  @Column({ type: 'character varying', name: 'last_name' })
+  lastName: string;
+
+  @Column({ type: 'character varying', name: 'document_id', unique: true })
+  documentId: string;
+
+  @Column({ type: 'character varying', unique: true })
+  email: string;
+
+  @Column({ type: 'character varying', name: 'phone_number', nullable: true })
+  phoneNumber: string;
+
+  @Column({ type: 'character varying' })
+  password: string;
+
+  @Column({ type: 'character varying', name: 'adress' })
+  adress: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.CUSTOMER })
+  role: UserRole;
+}
