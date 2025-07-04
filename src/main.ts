e78ffb87d0f4516551cca0719a289df3b93638e7
Abraham <1001.28021547.ucla@gmail.com>
2025-07-04 16:56:30 -0400

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,12 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN?.split(','),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
