424a7056836003523b6e8fa461b359b3b2b90535
Abraham <1001.28021547.ucla@gmail.com>
2025-07-07 16:42:22 -0400

    add category module complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ContractModule } from './contract/contract.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, AuthModule, ContractModule],
+  imports: [
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+    ContractModule,
+    CategoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/category/category.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
+import { CreateCategoryDTO, UpdateCategoryDTO } from './dto/category.dto';
+
+@Injectable()
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepo: Repository<Category>,
+  ) {}
+
+  async findAll(): Promise<Category[]> {
+    return this.categoryRepo.find();
+  }
+
+  async findOne(id: string): Promise<Category> {
+    const category = await this.categoryRepo.findOne({ where: { id } });
+    if (!category) {
+      throw new NotFoundException(`Category #${id} not found`);
+    }
+    return category;
+  }
+
+  async create(data: CreateCategoryDTO): Promise<Category> {
+    const category = this.categoryRepo.create(data);
+    return this.categoryRepo.save(category);
+  }
+
+  async update(id: string, data: UpdateCategoryDTO): Promise<Category> {
+    await this.categoryRepo.update(id, data);
+    return this.findOne(id);
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.categoryRepo.softDelete(id);
+  }
+}
