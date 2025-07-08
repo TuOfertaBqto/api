a97d3a128f1f51995b50c483dd1c47e603a0d1f2
Abraham <1001.28021547.ucla@gmail.com>
2025-07-08 15:32:23 -0400

    add contract-product entity

---
+++ src/contract/entities/contract-product.entity.ts
@@ -0,0 +1,32 @@
+import { BaseModel } from 'src/utils/entity';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Contract } from './contract.entity';
+import { Product } from 'src/product/entities/product.entity';
+
+export enum ContractProductStatus {
+  TO_BUY = 'to_buy',
+  TO_DISPATCH = 'to_dispatch',
+  DISPATCHED = 'dispatched',
+}
+
+@Entity('contract_product')
+export class ContractProduct extends BaseModel {
+  @ManyToOne(() => Contract)
+  @JoinColumn({ name: 'contract_id' })
+  contract: Contract;
+
+  @ManyToOne(() => Product)
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+
+  @Column({ type: 'timestamp', name: 'delivery_date' })
+  deliveryDate: Date;
+
+  @Column({
+    type: 'enum',
+    enum: ContractProductStatus,
+    default: ContractProductStatus.TO_BUY,
+    name: 'status',
+  })
+  status: ContractProductStatus;
+}
