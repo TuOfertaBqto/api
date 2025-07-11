a3ca66f5459c15a443f50571715562a9d2a045e3
Abraham <1001.28021547.ucla@gmail.com>
2025-07-11 17:37:57 -0400

    update inventory by product migration

--- src/inventory/entities/inventory-movement.entity.ts
+++ src/inventory/entities/inventory-movement.entity.ts
@@ -1,6 +1,6 @@
 import { BaseModel } from 'src/utils/entity';
 import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
-import { Inventory } from './inventory.entity';
+import { Product } from 'src/product/entities/product.entity';
 
 export enum MovementType {
   IN = 'in',
@@ -9,9 +9,9 @@ export enum MovementType {
 
 @Entity('inventory_movement')
 export class InventoryMovement extends BaseModel {
-  @ManyToOne(() => Inventory)
-  @JoinColumn({ name: 'inventory_id' })
-  inventory: Inventory;
+  @ManyToOne(() => Product)
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
 
   @Column({ type: 'enum', enum: MovementType, name: 'movement_type' })
   type: MovementType;
