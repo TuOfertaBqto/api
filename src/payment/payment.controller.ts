a23e488a6241894855ebb0d73c061d9904a895b0
Abraham <1001.28021547.ucla@gmail.com>
2025-10-03 11:42:10 -0400

    add payment crud

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { PaymentService } from './payment.service';
+import { Payment } from './entities/payment.entity';
 
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+  constructor(private readonly paymentService: PaymentService) {}
+
+  @Post()
+  create(@Body() data: Partial<Payment>) {
+    return this.paymentService.create(data);
+  }
+
+  @Get()
+  findAll() {
+    return this.paymentService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.paymentService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() data: Partial<Payment>) {
+    return this.paymentService.update(id, data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.paymentService.remove(id);
+  }
+}
