933a84e3b9442dc41d980f7574d3a1db46956c2f
Abraham <1001.28021547.ucla@gmail.com>
2025-10-03 10:45:32 -0400

    add payment module

---
+++ src/payment/entities/payment.entity.ts
@@ -0,0 +1,34 @@
+import { PaymentMethod } from 'src/installment/entities/installment.entity';
+import { BaseModel } from 'src/utils/entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity('payment')
+export class Payment extends BaseModel {
+  @Column({
+    type: 'enum',
+    enum: PaymentMethod,
+    name: 'type',
+    nullable: true,
+  })
+  type: PaymentMethod;
+
+  @Column({ type: 'int', name: 'reference_number', nullable: true })
+  referenceNumber: number;
+
+  @Column({ type: 'text', name: 'photo', nullable: true })
+  photo: string;
+
+  @Column({ type: 'varchar', name: 'owner', nullable: true })
+  owner: string;
+
+  @Column({
+    type: 'decimal',
+    name: 'amount',
+    precision: 10,
+    scale: 2,
+  })
+  amount: number;
+
+  @Column({ type: 'timestamp', name: 'paid_at' })
+  paidAt: Date;
+}
